70c16f4d58f7b3c392471097366c72ccfe9c403f
testlnord <testlnord@gmail.com>
2014-08-14 18:33:27 +0400

    Refactor logging

--- core/crawlers/sot_crawler.py
+++ core/crawlers/sot_crawler.py
@@ -3,20 +3,27 @@ import datetime
 import gzip
 import json
 import logging
-import re
-from ..config import config
 from ..utils.internet import internet
 
 
 __author__ = 'user'
 
 
+class OutOfApiQuotaException(Exception):
+    pass
+
+
 class SotCrawler:
-    @staticmethod
-    def get_data(tag, date_from, date_to=None) -> list:
+
+    def __init__(self, apikey):
+        self.apikey = apikey
+        self.logger = logging.getLogger(__name__)
+
+    def get_data(self, tag, date_from, date_to=None) -> list:
         if date_to is None:
-            date_to = datetime.datetime.now().date()
+            date_to = datetime.datetime.today()
         date_from -= datetime.timedelta(days=-date_from.weekday())  # getting monday date
+
         tag = internet.quote(tag.replace(" ", "-"))
         result = []
         week = date_from
@@ -26,18 +33,22 @@ class SotCrawler:
                   "fromdate={0}&" \
                   "todate={1}&" \
                   "key={2}&" \
-                  "order=desc&sort=activity&site=stackoverflow&filter=!)V)MSZJYs)y".format(
-                  int(week.timestamp()), int(next_week.timestamp()), internet.quote(config["sources"]["so"]["apikey"])
+                  "order=desc&sort=activity&site=stackoverflow&filter=!GeF-5sJS7jmzL".format(
+                  int(week.timestamp()), int(next_week.timestamp()), internet.quote(self.apikey)
                   )
-            url += "&tagged={0}".format(tag)
-            logging.debug("Generated url fo SO: " + url)
-            data = internet.get_from_url(url, 3, 7, binary=True)
+            if tag:
+                url += "&tagged={0}".format(tag)
+            self.logger.debug("Generated url fo SO: " + url)
+            data = internet.get_from_url(url, 1, 4, binary=True, force_wait=True)
             data = gzip.decompress(data)
             response = json.loads(data.decode())
+            self.logger.debug("Response: %s", data.decode())
             count = response["total"]
-
+            if response["quota_remaining"] < 10:
+                self.logger.warning("Stackoveflow crawler is out of quota.")
+                raise OutOfApiQuotaException
             result.append((week, count))
-            print(' ', result[-1])
+            self.logger.debug('Data portion last line: %s',  str(result[-1]))
 
             internet.sleep(1, 2)
 
