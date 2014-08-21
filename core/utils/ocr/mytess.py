0090573599a3e498d2cc07f7b6c93e4055945ada
testlnord <testlnord@gmail.com>
2014-08-21 11:38:32 +0400

    Poor people OCR my tess works now without tesseract

--- core/utils/ocr/mytess.py
+++ core/utils/ocr/mytess.py
@@ -9,7 +9,7 @@ __author__ = 'user'
 
 import subprocess
 import io
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy
 
 
@@ -17,39 +17,33 @@ def eqdist(vec1, vec2):
     return numpy.linalg.norm(numpy.array(vec1) - numpy.array(vec2))
 
 
-
 def tess_percents(image):
     """image -- PIL or Pillow image
-    requires tricky installation of tesseract
     """
-    #todo remove tesseract from requirements
-    image = image.resize(tuple(x*2 for x in image.size), Image.BICUBIC)
-    file = io.BytesIO()
-    image.save(file, 'PNG')
-    subp = subprocess.Popen(['tesseract', 'stdin', 'stdout', '-psm=8', 'bazaar'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    res = subp.communicate(input=file.getvalue())
-    res = res[0].decode()
-    res = res.splitlines()[0]
-    res = res.replace(' ', '')
-    if res[-1] != '%' and res.endswith('96'):
-        res = res[:-2] + '%'
-    return res
-
-
-def tess_number(image):
-    """image -- PIL or Pillow image"""
-    image = image.resize(tuple(x*2 for x in image.size), Image.BICUBIC)
-    file = io.BytesIO()
-    image.save(file, 'PNG')
-    subp = subprocess.Popen(['tesseract', 'stdin', 'stdout', '-psm=8', 'digits'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    res = subp.communicate(input=file.getvalue())
-    print(res[0])
-    return res[0].decode()
+    text = image.crop(ImageOps.invert(image.convert('RGB')).getbbox())
+    left_unrecognized_border = 0
+    glyph_path = os.path.join(os.path.dirname(__file__), 'digit_glyphs', 'p9')
+    line = ""
+    while left_unrecognized_border < text.size[0]:
+        # recognize one symbol
+        similarity = []
+        for glyph_file in os.listdir(glyph_path):
+            glyph = Image.open(os.path.join(glyph_path, glyph_file))
+            if glyph.size[0] > text.size[0] - left_unrecognized_border:
+                continue
+            h1 = text.crop(
+                (left_unrecognized_border, 0, left_unrecognized_border + glyph.size[0], text.size[1])).histogram()
+            h2 = glyph.histogram()
+            similarity.append((glyph_file[0], eqdist(h1, h2), glyph.size[0]))
+        symbol = min(similarity, key=lambda x: x[1])
+        line += symbol[0]
+        left_unrecognized_border += symbol[2]
+    return line
 
 
 def tess_digit(image):
     """image -- PIL or Pillow image"""
-    glyph_path = os.path.join(os.path.dirname(__file__), 'digit_glyphs')
+    glyph_path = os.path.join(os.path.dirname(__file__), 'digit_glyphs', 'p8')
     values = []
 
     h1 = image.histogram()
