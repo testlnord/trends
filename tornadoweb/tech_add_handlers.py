e48e269e9cecbcdffe705084d39d4a6ad8c3195d
testlnord <testlnord@gmail.com>
2015-09-11 11:38:38 +0300

    Remove config from output on technology adding

--- tornadoweb/tech_add_handlers.py
+++ tornadoweb/tech_add_handlers.py
@@ -33,52 +33,43 @@ class AddFormHandler(tornado.web.RequestHandler):
         self.write(page_template.generate())
 
     def post(self):
+        self.write("<html><body><div>")
+        tech_name = self.get_argument("tech_name", "")
+        wiki_pages = self.get_argument("wiki_all", "")
+        google_name = self.get_argument("google_name", "")
+        sot_tag = self.get_argument("sot_tag", "")
+        itj_page = self.get_argument("itj_name", "")
+        self.write("tn: " + tech_name + "<br/>\n")
+        self.write("wn: " + wiki_pages + "<br/>\n")
+        self.write("gn: " + google_name + "<br/>\n")
+        self.write("sn: " + sot_tag + "<br/>\n")
+        self.write("in: " + itj_page + "<br/>\n")
 
-        try:
-            self.write("<html><body><div>")
-            tech_name = self.get_argument("tech_name", "")
-            wiki_pages = self.get_argument("wiki_all", "")
-            google_name = self.get_argument("google_name", "")
-            sot_tag = self.get_argument("sot_tag", "")
-            itj_page = self.get_argument("itj_name", "")
-            self.write("tn: " + tech_name + "<br/>\n")
-            self.write("wn: " + wiki_pages + "<br/>\n")
-            self.write("gn: " + google_name + "<br/>\n")
-            self.write("sn: " + sot_tag + "<br/>\n")
-            self.write("in: " + itj_page + "<br/>\n")
-            self.write("<br/>" + "-"*60 + "<br/>\n")
-            self.write(config)
-            self.write("<br/>" + "-"*60 + "<br/>\n")
+        if not tech_name or (not wiki_pages and
+                                 not google_name and
+                                 not sot_tag and
+                                 not itj_page):
+            self.write("failed. Nothing to insert")
+            return
 
-            if not tech_name or (not wiki_pages and
-                                     not google_name and
-                                     not sot_tag and
-                                     not itj_page):
-                self.write("failed. Nothing to insert")
-                return
-
-            cur = self.db_connection.cursor()
-            cur.execute("insert into techs(info) values(%s) returning id", (Json({'name': tech_name}),))
-            self.db_connection.commit()
-            fb_id = cur.fetchone()[0]
-            if wiki_pages:
-                wiki_pages = wiki_pages.split(",")
-                WikiUpdater().add_new_tech(fb_id, wiki_pages)
-                self.write("wiki updated\n<br/>")
-            if sot_tag:
-                SotUpdater().add_new_tech(fb_id, sot_tag)
-                self.write("sot updatd\n<br/>")
-            if google_name:
-                GoogleUpdater().add_new_tech(fb_id, google_name)
-                self.write("google updated\n<br/>")
-            if itj_page:
-                ItjUpdater().add_new_tech(fb_id, google_name)
-                self.write("Itj upd \n<br/>")
-            self.write("Inserted successfully")
-        except Exception:
-            self.write("Exception in user code:")
-            self.write("-"*60)
-            self.write(traceback.format_exc())
+        cur = self.db_connection.cursor()
+        cur.execute("insert into techs(info) values(%s) returning id", (Json({'name': tech_name}),))
+        self.db_connection.commit()
+        fb_id = cur.fetchone()[0]
+        if wiki_pages:
+            wiki_pages = wiki_pages.split(",")
+            WikiUpdater().add_new_tech(fb_id, wiki_pages)
+            self.write("wiki updated\n<br/>")
+        if sot_tag:
+            SotUpdater().add_new_tech(fb_id, sot_tag)
+            self.write("sot updatd\n<br/>")
+        if google_name:
+            GoogleUpdater().add_new_tech(fb_id, google_name)
+            self.write("google updated\n<br/>")
+        if itj_page:
+            ItjUpdater().add_new_tech(fb_id, google_name)
+            self.write("Itj upd \n<br/>")
+        self.write("Inserted successfully")
         self.write("</div></body></html>")
 
 
